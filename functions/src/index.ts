f62d2019f4634a68889056e8bad7324524bdf0a8
Nitzan Hen <code@nitzanhen.net>
2021-10-14 11:07:36 +0000

    add region to deploy to eurpoe

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import {https} from "firebase-functions";
+import {region} from "firebase-functions";
 import {initializeApp} from "firebase/app";
 import {collection, doc, getDoc, getFirestore} from "firebase/firestore";
 import firebaseConfig from "./firebase-config.json";
@@ -8,29 +8,30 @@ const db = getFirestore();
 
 const urls = collection(db, "urls");
 
-export const redirect = https.onRequest(async (req, res) => {
-  try {
-    const urlId = req.params[0];
-    if (!urlId || urlId === "/") {
-      res.status(400).send("Specify a url");
-      return;
-    }
+export const redirect = region("eurpose-west1")
+    .https.onRequest(async (req, res) => {
+      try {
+        const urlId = req.params[0];
+        if (!urlId || urlId === "/") {
+          res.status(400).send("Specify a url");
+          return;
+        }
 
-    const docRef = doc(urls, urlId);
-    const docSnap = await getDoc(docRef);
+        const docRef = doc(urls, urlId);
+        const docSnap = await getDoc(docRef);
 
-    const url = docSnap.get("url");
+        const url = docSnap.get("url");
 
-    if (!url) {
-      res.sendStatus(404);
-      return;
-    } else if (typeof url !== "string") {
-      throw new Error("unexpected non-string url");
-    }
+        if (!url) {
+          res.sendStatus(404);
+          return;
+        } else if (typeof url !== "string") {
+          throw new Error("unexpected non-string url");
+        }
 
-    res.redirect(301, url);
-  } catch (e) {
-    console.error(e);
-    res.sendStatus(500);
-  }
-});
+        res.redirect(301, url);
+      } catch (e) {
+        console.error(e);
+        res.sendStatus(500);
+      }
+    });
