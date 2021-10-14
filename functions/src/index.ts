5c1439d5db86be21def6c0603bf29ba3d7eff4cf
Nitzan Hen <code@nitzanhen.net>
2021-10-14 10:54:03 +0000

    Implemented what I had in the previous implementation

---
+++ functions/src/index.ts
@@ -0,0 +1,36 @@
+import {https} from "firebase-functions";
+import {initializeApp} from "firebase/app";
+import {collection, doc, getDoc, getFirestore} from "firebase/firestore";
+import firebaseConfig from "./firebase-config.json";
+
+initializeApp(firebaseConfig);
+const db = getFirestore();
+
+const urls = collection(db, "urls");
+
+export const redirect = https.onRequest(async (req, res) => {
+  try {
+    const urlId = req.params[0];
+    if (!urlId || urlId === "/") {
+      res.status(400).send("Specify a url");
+      return;
+    }
+
+    const docRef = doc(urls, urlId);
+    const docSnap = await getDoc(docRef);
+
+    const url = docSnap.get("url");
+
+    if (!url) {
+      res.sendStatus(404);
+      return;
+    } else if (typeof url !== "string") {
+      throw new Error("unexpected non-string url");
+    }
+
+    res.redirect(301, url);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
+});
